3a201060ad55a45ddc0ce7ebab50c0501faef50c
Dilip Nepali <er.dilip.n@gmail.com>
2022-12-18 02:37:45 -0500

    fix: import icon

--- components/layout/header/Navbar.jsx
+++ components/layout/header/Navbar.jsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 
 import { useState } from 'react';
 import { Logo, NavItem, DarkMode } from './'
-import Burger from '../../icons/Burger';
 
 const MENU_LIST = [
   { text: "Home", href: "/home", active: true },
